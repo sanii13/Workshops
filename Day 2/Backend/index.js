2b16f28bf0d13b31e9634e6b0af4fabb025f7a2e
sanii13 <sanidi1113@gmail.com>
2024-04-03 19:07:55 +0530

    Function

---
+++ Day 2/Backend/index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+
+  // Set CORS headers
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader("Content-Type", "text/plain");
+
+  res.end("Hello\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
